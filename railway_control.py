ea44fb3653f2415184c782d5d5d40e1727fba209
Oskar Sąkol <sakoloskar@gmail.com>
2020-08-18 17:03:14 +0200

    Railway control inrerface implementation

--- railway_control.py
+++ railway_control.py
@@ -5,12 +5,35 @@ import random
 logger = logging.getLogger(__name__)
 
 
-def establish_route(_id: int):
+def establish_route(_id: int, generated_route, route_length):
     logger.info(f"trying to establish route {_id}!")
 
+    for course in range(route_length):
 
-def release_route(_id: int):
+        starting_semaphore_signal = get_signal_state(generated_route[course][1])
+        finishing_semaphore_signal = get_signal_state(generated_route[course][2])
+
+        if starting_semaphore_signal != "SX" or finishing_semaphore_signal != "S1":
+            note = "Failed at Establishing"
+            return False, note
+        note = "Passed at Establishing"
+        return True, note
+
+
+def release_route(_id: int, generated_route, route_length):
     logger.info(f"trying to release route {_id}!")
+
+    for course in range(route_length, 0, -1):
+
+        starting_semaphore_signal = get_signal_state(generated_route[course - 1][1])
+        finishing_semaphore_signal = get_signal_state(generated_route[course - 1][2])
+
+        if starting_semaphore_signal != "S1" or finishing_semaphore_signal != "S1":
+            note = "Failed at Releasing"
+            return False, note
+
+    note = "Passed at Releasing"
+    return True, note
 
 
 def get_signal_state(name: str):
--- signal_test.py
+++ signal_test.py
@@ -16,8 +16,8 @@ def signal_test(generated_routes, db):
 
         [testing_courses_ids.append(generated_routes[key][course][0]) for course in range(route_length)]
 
-        establish_test, establish_note = establish_route_test(generated_routes[key], route_length)
-        release_test, release_note = release_route_test(generated_routes[key], route_length)
+        establish_test, establish_note = establish_route(key, generated_routes[key], route_length)
+        release_test, release_note = release_route(key, generated_routes[key], route_length)
 
         note = establish_note + ", " + release_note
 
@@ -31,28 +31,28 @@ def signal_test(generated_routes, db):
             conn.commit()
 
 
-def establish_route_test(generated_route, route_length):
-
-    for course in range(route_length):
-        establish_route(generated_route[course][0])
-        starting_semaphore_signal = get_signal_state(generated_route[course][1])
-        finishing_semaphore_signal = get_signal_state(generated_route[course][2])
-        if starting_semaphore_signal != "SX" or finishing_semaphore_signal != "S1":
-            note = "Failed at Establishing"
-            return False, note
-        note = "Passed at Establishing"
-        return True, note
-
-
-def release_route_test(generated_route, route_length):
-
-    for course in range(route_length, 0, -1):
-
-        release_route(generated_route[course - 1][0])
-        starting_semaphore_signal = get_signal_state(generated_route[course - 1][1])
-        finishing_semaphore_signal = get_signal_state(generated_route[course - 1][2])
-        if starting_semaphore_signal != "S1" or finishing_semaphore_signal != "S1":
-            note = "Failed at Releasing"
-            return False, note
-    note = "Passed at Releasing"
-    return True, note
+# def establish_route_test(generated_route, route_length):
+#
+#     for course in range(route_length):
+#         establish_route(generated_route[course][0])
+#         starting_semaphore_signal = get_signal_state(generated_route[course][1])
+#         finishing_semaphore_signal = get_signal_state(generated_route[course][2])
+#         if starting_semaphore_signal != "SX" or finishing_semaphore_signal != "S1":
+#             note = "Failed at Establishing"
+#             return False, note
+#         note = "Passed at Establishing"
+#         return True, note
+#
+#
+# def release_route_test(generated_route, route_length):
+#
+#     for course in range(route_length, 0, -1):
+#
+#         release_route(generated_route[course - 1][0])
+#         starting_semaphore_signal = get_signal_state(generated_route[course - 1][1])
+#         finishing_semaphore_signal = get_signal_state(generated_route[course - 1][2])
+#         if starting_semaphore_signal != "S1" or finishing_semaphore_signal != "S1":
+#             note = "Failed at Releasing"
+#             return False, note
+#     note = "Passed at Releasing"
+#     return True, note
